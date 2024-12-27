cf5fea027354a5dffe990261b59e9f01fe56eea9
Lars Verhaeghe <148888518+larsv2801@users.noreply.github.com>
2024-12-27 17:54:37 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open('v1').then((cache) => {
       return cache.addAll([
-        '/kersgame/index.html',
+        '/kerstgame/index.html',
         '/kerstgame/android-chrome-192x192.png',
         '/kerstgame/android-chrome-512x512.png',
         '/kerstgame/Lofi.mp3',
